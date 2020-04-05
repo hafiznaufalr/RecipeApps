122c3a9fe70226848c4e9d556555d90aa5ea6509
hafiz <hafiz.nr5758@gmail.com>
2020-04-05 12:06:33 +0700

    change minor

--- app/src/main/java/net/hafiznaufalr/recipeapps/ui/main/CategoryRecipeAdapter.kt
+++ app/src/main/java/net/hafiznaufalr/recipeapps/ui/main/CategoryRecipeAdapter.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_category.view.*
--- app/src/main/java/net/hafiznaufalr/recipeapps/ui/main/MainContract.kt
+++ app/src/main/java/net/hafiznaufalr/recipeapps/ui/main/MainContract.kt
@@ -1,7 +1,6 @@
 package net.hafiznaufalr.recipeapps.ui.main
 
 import net.hafiznaufalr.recipeapps.model.CategoryResponse
-import net.hafiznaufalr.recipeapps.model.Recipe
 import net.hafiznaufalr.recipeapps.model.RecipeResponse
 import net.hafiznaufalr.recipeapps.ui.base.BasePresenter
 import net.hafiznaufalr.recipeapps.ui.base.BaseView
